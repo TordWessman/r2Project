167bf60646a50b31c8949d6660a8cfdeb2061551
Tord Wessman <tord@axelit.se>
2020-10-27 21:30:03 +0100

    Audio: indentation fixes, change gst mp3 decoder, make base path optional for mp3 player.

--- Src/Audio/Native/r2mp3.c
+++ Src/Audio/Native/r2mp3.c
@@ -27,7 +27,8 @@ GMemoryInputStream *mistream;
 
 static GstElement *mp3_bin, 	// the containing all the elements
 		*mp3_pipeline, 	 		// the pipeline for the bin
-		*mp3_src, 	 		
+		*mp3_src,
+		*mp3_parser, 	 		
 		*mp3_decoder, 
 		*mp3_echo,	
 		*mp3_alsa_sink;
@@ -38,33 +39,34 @@ static int mp3_is_playing = 0; //indicates that the player is busy
 
 static int mp3_is_initialized = 0;
 
-static gboolean mp3_bus_call(GstBus *bus, GstMessage *msg, void *user_data)
-{
+static gboolean mp3_bus_call(GstBus *bus, GstMessage *msg, void *user_data) {
 
 	switch (GST_MESSAGE_TYPE(msg)) {
-	case GST_MESSAGE_EOS: {
-		g_main_loop_quit(mp3_loop);
-		break;
-	}
-	case GST_MESSAGE_ERROR: {
-		GError *err;
-		gst_message_parse_error(msg, &err, NULL);
-		//report error
-		printf ("ERROR: %s", err->message);
-		g_error_free(err);
+
+		case GST_MESSAGE_EOS: {
+
+			g_main_loop_quit(mp3_loop);
+			break;
 		
-		g_main_loop_quit(mp3_loop);
-		
-		break;
-	} 
-	default:
-	
-		break;
+		} case GST_MESSAGE_ERROR: {
+
+			GError *err;
+			gst_message_parse_error(msg, &err, NULL);
+			//report error
+			printf ("ERROR: %s", err->message);
+			g_error_free(err);
+			
+			g_main_loop_quit(mp3_loop);
+			
+			break;
+		} default: { break;}
+
 	}
 
 		//printf("info: %i %s\n", (int)(msg->timestamp), GST_MESSAGE_TYPE_NAME (msg));
 
 	return true;
+
 }
 
 //this method initiates the gobjects, the pipeline, the bus and the entire bin
@@ -80,7 +82,8 @@ int mp3_init(GstElement *src) {
 
 	//initializing elements
 	mp3_src = src;
-	mp3_decoder = gst_element_factory_make ("mad", "decoder");
+	mp3_parser = gst_element_factory_make ("mpegaudioparse", "parser");
+	mp3_decoder = gst_element_factory_make ("mpg123audiodec", "decoder");
 	mp3_alsa_sink = gst_element_factory_make ("alsasink", "alsasink");
 	mp3_echo = gst_element_factory_make("audioecho", "echo");
 	//g_object_set (G_OBJECT (mp3_echo), "delay", 9000000, NULL);
@@ -91,7 +94,12 @@ int mp3_init(GstElement *src) {
 		g_critical("Unable to create filesrc!\n");
 		return false;
 
-	} else if(!mp3_decoder) {
+	} else if(!mp3_parser) {
+
+		g_critical( "Unable create mp3 parser\n");
+		return false;
+
+	}  else if(!mp3_decoder) {
 
 		g_critical( "Unable create mp3 decoder\n");
 		return false;
@@ -118,6 +126,7 @@ int mp3_init(GstElement *src) {
 	// Add the elements to the bin
 	gst_bin_add_many (GST_BIN (mp3_bin), 
 		mp3_src,
+		mp3_parser,
 		mp3_decoder, 
 		//echo,
 		mp3_alsa_sink,
@@ -128,7 +137,8 @@ int mp3_init(GstElement *src) {
 
 	// link the elements and check for success
 	if (!gst_element_link_many (
-		mp3_src, 
+		mp3_src,
+		mp3_parser,
 		mp3_decoder,
 		//echo,
 		mp3_alsa_sink,
@@ -149,16 +159,20 @@ int mp3_init(GstElement *src) {
 
 //stop the main loop and dealocate resources
 void mp3_stop() {
+
 	gst_element_set_state(GST_ELEMENT(mp3_pipeline), GST_STATE_NULL);
 	g_main_loop_quit(mp3_loop);
 	gst_object_unref(GST_OBJECT(mp3_pipeline));
 	g_main_loop_unref (mp3_loop);
+
 }
 
 // Just stops the playback
 void mp3_stop_playback() {
+
 	gst_element_set_state(GST_ELEMENT(mp3_pipeline), GST_STATE_NULL);
 	g_main_loop_quit(mp3_loop);
+
 }
 
 void mp3_play() {
@@ -174,10 +188,11 @@ void mp3_play() {
 void mp3_file_play(const char* mp3_file) {
 
 	if (!mp3_is_initialized) {
+
 		g_critical ("Mp3 player Unable to play. You have to initialize first!");
 		return; 
+	
 	}
-
 
 	mp3_is_playing = 1;
 	g_object_set (G_OBJECT (mp3_src), "location", mp3_file, NULL);
@@ -214,7 +229,9 @@ void mp3_memory_play(uint8_t* mp3_pointer, int size) {
 }
 
 void _ext_stop_mp3() {
+
 	mp3_stop();
+
 }
 
 void _ext_stop_playback_mp3() {
@@ -248,15 +265,21 @@ void _ext_play_memory_mp3(uint8_t* mp3_pointer, int size) {
 }
 
 void _ext_pause_mp3() {
+
 	g_main_loop_quit(mp3_loop);
+
 }
 
 int _ext_is_playing_mp3 () {
+
 	return mp3_is_playing;
+
 }
 
 int _ext_is_initialized_mp3 () {
+
 	return mp3_is_initialized;
+
 }
 
 int main (int argc, char *argv[]) {
@@ -269,15 +292,20 @@ int main (int argc, char *argv[]) {
 			mp3_file_play (argv[1]);
 			mp3_stop();
 
-		}
-		else 
+		} else {
+
 			printf ("Nothing to play...");
-	}
-	else {
+		
+		}
+	
+	} else {
+	
 		printf ("unable to initialize");
 		return -1;
+	
 	}
 	
 	return 0;
+
 }
 
