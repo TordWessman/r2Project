af5fd131af80bbdc7f23c20b21be8c8ccb0a6c1b
Tord Wessman <tord@axelit.se>
2020-10-27 21:27:20 +0100

    Update assembly naming and clean up project structure

--- Src/Common/Properties/AssemblyInfo.cs
+++ Src/Common/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Common")]
+[assembly: AssemblyTitle("R2Core.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
