fd5df0e32879036847b9f49a4e0709c2cdce6276
Tord Wessman <tord@axelit.se>
2024-04-04 22:33:56 +0200

    Fix script_headers: Make Audio and Video optional and remove PushNotifications

--- Common/script_header.py
+++ Common/script_header.py
@@ -1,20 +1,24 @@
 import clr
+import os
+audio_lib = "R2Core.Audio.dll"
+video_lib = "R2Core.Video.dll"
+
 clr.AddReferenceToFileAndPath(r"R2Core.dll")
 clr.AddReferenceToFileAndPath(r"R2Core.Common.dll")
-clr.AddReferenceToFileAndPath(r"R2Core.PushNotifications.dll")
 clr.AddReferenceToFileAndPath(r"R2Core.Scripting.dll")
-clr.AddReferenceToFileAndPath(r"R2Core.Video.dll")
-clr.AddReferenceToFileAndPath(r"R2Core.Audio.dll")
+
 from R2Core import *
-import R2Core.PushNotifications
 import R2Core.Scripting
 import R2Core.Common
-import R2Core.Video
-import R2Core.Audio
 clr.ImportExtensions(R2Core.Common)
 clr.ImportExtensions(R2Core)
 clr.ImportExtensions(R2Core.Network)
-clr.ImportExtensions(R2Core.PushNotifications)
 clr.ImportExtensions(R2Core.Scripting)
-clr.ImportExtensions(R2Core.Video)
-clr.ImportExtensions(R2Core.Audio)
+
+if os.path.isfile(audio_lib):
+	clr.AddReferenceToFileAndPath(audio_lib)
+	clr.ImportExtensions(R2Core.Audio)
+
+if os.path.isfile(video_lib):
+	clr.AddReferenceToFileAndPath(video_lib)
+	clr.ImportExtensions(R2Core.Video)
--- Src/Scripting/IronScript/IronScript.cs
+++ Src/Scripting/IronScript/IronScript.cs
@@ -18,7 +18,6 @@
 //
 using System;
 using System.IO;
-using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 using System.Collections.Generic;
 using System.Linq;
