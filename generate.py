0ca4d05db74589bc5f571047e569f11588ce10b9
Tord Wessman <tord@axelit.se>
2024-04-05 14:05:22 +0200

    Boilerplate genartion bug fixes

--- generate.py
+++ generate.py
@@ -1,15 +1,17 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import os
 import argparse
 import shutil
 
 BOILERPLATE_PATH = "./Boilerplate"
-EXTENSIONS = ['.csproj', '.cs', 'sln'] 
+EXTENSIONS = ['.csproj', '.cs', 'sln', 'csproj.user'] 
 BOILERPLATE_NAME = "r2Boilerplate"
 RESOURCES_NAME = "Resources"
+CORE_CONFIGURATION = "Core.config"
 PROJECT_DIR = os.path.join(BOILERPLATE_PATH, BOILERPLATE_NAME)
 RESOURCES_DIR = os.path.join(BOILERPLATE_PATH, RESOURCES_NAME)
+CORE_CONFIGURATION_FILE = os.path.join(BOILERPLATE_PATH, CORE_CONFIGURATION)
 
 def replace_in_files(directory, in_str, replace_str, extensions):
 
@@ -19,19 +21,16 @@ def replace_in_files(directory, in_str, replace_str, extensions):
 			if any(file.endswith(ext) for ext in extensions):
 				file_path = os.path.join(root, file)
 
-			with open(file_path, 'r') as f:
-				content = f.read()
+				with open(file_path, 'r') as f:
+					content = f.read()
 
-			content = content.replace(replace_str, in_str)
+				content = content.replace(replace_str, in_str)
 
-			with open(file_path, 'w') as f:
-				f.write(content)
+				with open(file_path, 'w') as f:
+					f.write(content)
 
-			new_file_path = os.path.join(root, file.replace(replace_str, in_str))
-			os.rename(file_path, new_file_path)
-
-def copy_directory(src_dir, dest_dir):
-	shutil.copytree(src_dir, dest_dir)
+				new_file_path = os.path.join(root, file.replace(replace_str, in_str))
+				os.rename(file_path, new_file_path)
 
 if __name__ == "__main__":
 
@@ -43,10 +42,12 @@ if __name__ == "__main__":
 	parent_directory = ".."
 	project_target_directory = os.path.join(parent_directory, project_name)
 	resources_target_directory = os.path.join(parent_directory, RESOURCES_NAME)
+	core_configuration_target = os.path.join(parent_directory, CORE_CONFIGURATION)
 
-	copy_directory(PROJECT_DIR, project_target_directory)
-	copy_directory(RESOURCES_DIR, resources_target_directory)
+	shutil.copytree(PROJECT_DIR, project_target_directory)
+	shutil.copytree(RESOURCES_DIR, resources_target_directory)
+	shutil.copy(CORE_CONFIGURATION_FILE, core_configuration_target)
 
-	print("Generating project in {project_target_directory}")
+	print(f"Generating project in {project_target_directory}")
 
-	replace_in_files(project_target_directory, BOILERPLATE_NAME, project_name, EXTENSIONS)
+	replace_in_files(project_target_directory, project_name, BOILERPLATE_NAME, EXTENSIONS)
